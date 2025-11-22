08655d24c4776ff32ba89867d85bd5b844cbd3cc
Ameer Khalil <ameerkhalil795@gmail.com>
2025-11-22 17:11:29 -0500

    Remove ignored files and update

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['unicorn-images.b-cdn.net'],
+  },
+}
+
+module.exports = nextConfig
